459658198550f697e7295c72e2d67daad9c1e187
jashan777 <js400951@gmail.com>
2022-11-17 19:26:41 +0530

    fix in Homepage to integrate banner

--- client/redcross-webapp/src/homepage/homepage.js
+++ client/redcross-webapp/src/homepage/homepage.js
@@ -10,8 +10,6 @@ import Navbar from "./navbar/navbar";
 //below banner(carousel) imports 
 import Imageslider from "./banner/imagesliderv3";
 
-import { sliderdata } from "./banner/sliderdata";
-
 //official info imports
 import Officialinfo from "./officalinfo/Officialinfov2";
 
@@ -375,7 +373,7 @@ const Homepage = () => {
 
                     <div className="carousel">
 
-                        <Imageslider slides={sliderdata} />
+                        <Imageslider/>
 
                     </div>
 
